3250f6734a349cf3b1bc026832d12025fe0b9b5e
luthando7 <luthandozaze@gmail.com>
2020-10-09 22:39:13 +0200

    changes

--- wristband/dashboard/urls.py
+++ wristband/dashboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home-page'),
+    path('dashboard', views.dashboard, name='dashboard-page'),
+    path('maps_google', views.maps_google, name='maps-page')
 ]
--- wristband/dashboard/views.py
+++ wristband/dashboard/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'dashboard/index.html')
+
+def dashboard(request):
+    return render(request, 'dashboard/dashboard.html')
+
+def maps_google(request):
+    return render(request, 'dashboard/map-google.html')
