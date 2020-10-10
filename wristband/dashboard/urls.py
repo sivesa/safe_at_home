f4b1c61ee508bff28d319baad7aba43e60727d27
Sive Sa <sivesandla@live.com>
2020-10-10 15:24:09 +0200

    Update urls.py

--- wristband/dashboard/urls.py
+++ wristband/dashboard/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home-page'),
     path('dashboard', views.dashboard, name='dashboard-page'),
-    path('maps_google', views.maps_google, name='maps-page')
+    path('maps_google', views.maps_google, name='maps-page'),
+    path('page_404', views.page404, name='404-page'),
 ]
